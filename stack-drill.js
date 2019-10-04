5f8a5c24cbedc7c80935baf462e7e1e13364c8d1
Fabian Lema <fabianlemac@gmail.com>
2019-10-04 11:07:25 -0500

    matching

--- stack-drill.js
+++ stack-drill.js
@@ -62,8 +62,41 @@ function isPalindrome(str){
   return true
 }
 
-console.log(isPalindrome('dad'))
+/*console.log(isPalindrome('dad'))
 console.log(isPalindrome('A man, a plan, a canal: Panama'))
 console.log(isPalindrome('1001'))
-console.log(isPalindrome('Tauhida'))
+console.log(isPalindrome('Tauhida'))*/
 
+function matchingExpression(expression){
+  let stack = new Stack();
+  for (let i = 0; i < expression.length; i++){
+    if (expression[i]==='(' || expression[i]===')')
+      stack.push(expression[i])
+  }
+  //display(stack)
+  let lastParenthesis = stackPeek(stack);
+  //console.log(lastParenthesis);
+  for (let i = 0; i < expression.length-1; i++){
+    if (expression[i]==='('){
+      stack.pop()
+    }
+      //stack.pop()
+  }
+  display(stack)
+  if (lastParenthesis === ')' && stackPeek(stack) === '(') return 'The expression is matching'
+  else if (lastParenthesis !== ')' && stackPeek(stack) !== '(') return 'The expression is missing a "(" and ")"'
+  else if (lastParenthesis !== ')') return 'The expression is missing a "("'
+  else return 'The expression is missing a ")"'
+
+}
+
+console.log(matchingExpression('345+(434)+34'));
+console.log(matchingExpression('345+{434)+34'));
+/*console.log(matchingExpression('345+(434}+34'));
+console.log(matchingExpression('345+{434}+34'));*/
+
+
+/*
+)
+{
+*/
